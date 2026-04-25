288e4b7adf743efaba41bb6a242ddd0dd32178f5
Kailash_Gurumoorthi <gkailash.vs@gmail.com>
2026-04-25 17:47:43 +0530

    reverse any given number recursively

---
+++ codings/ReverseRecue.java
@@ -0,0 +1,15 @@
+public class ReverseRecue {
+    //Reverse a number using Recursion
+    public static void main(String[] args){
+        int num=102;
+        recur(num);
+    }
+    public static void recur(int num){
+        if(num<10){
+            System.out.print(num);
+            return;
+        }
+        System.out.print(num%10);
+        recur(num/10);
+    }
+}
