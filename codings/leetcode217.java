dca4fc51b79cab5608f5363154e14bd110c89a46
Kailash_Gurumoorthi <gkailash.vs@gmail.com>
2026-06-03 16:20:00 +0530

    think about all the approaches then code , don't start coding instantly when you got one approach

---
+++ codings/leetcode217.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class leetcode217 {
+    public static void main(String[] args){
+        int[] nums={1,2,3,1};
+        boolean result = containsDuplicate(nums);
+        System.out.print(result);
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
