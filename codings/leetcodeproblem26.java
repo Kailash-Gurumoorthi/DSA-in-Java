61f75cf0614f52c126deded53dcacf7db8a8efa3
Kailash_Gurumoorthi <gkailash.vs@gmail.com>
2026-06-05 22:17:40 +0530

    leet code problem 26

---
+++ codings/leetcodeproblem26.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class leetcodeproblem26 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2 ,3};
+        System.out.println(nondupli(nums));
+    }
+    public static int nondupli(int[] nums){
+        if(nums == null || nums.length == 0) return 0;
+        /* int i=0,j=1;
+        while(j<nums.length){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+            j++;
+        }
+        System.out.println(Arrays.toString(nums));
+        return i+1; */
+        int i=1;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j-1]!=nums[i]){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
