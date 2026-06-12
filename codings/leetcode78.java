a740233672c325118cffbca6c4bd429690b0c341
Kailash_Gurumoorthi <gkailash.vs@gmail.com>
2026-06-12 22:47:51 +0530

    think think think always it's a simple logic kailash so think

---
+++ codings/leetcode78.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+public class leetcode78 {
+    public static void main(String[] args){
+        int[] nums={1,2,3};
+        System.out.print(subsets(nums));
+    }
+    public static List<List<Integer>> subsets(int[] nums){
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList<>());
+        for(int num : nums){
+            int size=list.size();
+            for(int i = 0;i<= size-1;i++){
+                List<Integer> temp = new ArrayList<>(list.get(i));
+                temp.add(num);
+                list.add(temp);
+            }
+        }
+        return list;
+    }
+}
