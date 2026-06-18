86e4614ebcd64ac3108f2a1ceb07e4a0a73446c1
Kailash_Gurumoorthi <gkailash.vs@gmail.com>
2026-06-18 21:02:22 +0530

    Contains Duplicate II

---
+++ codings/hashing/leetcode219.java
@@ -0,0 +1,52 @@
+package hashing;
+
+/*
+
+contains duplicate II
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+*/
+
+import java.util.*;
+public class leetcode219 {
+    public static void main(String[] args){
+        int[] nums = {1,2,3,1};
+        int k = 3;
+        System.out.print(containsNearbyDuplicate(nums,k));
+    }
+    static boolean containsNearbyDuplicate(int[] nums, int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <=k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
